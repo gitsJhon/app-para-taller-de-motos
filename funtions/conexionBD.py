21a6f9fef4d2248b330a4883079ba70b24aa0311
gitsJhon <sebas2002jhon@gmail.com>
2025-01-16 15:06:58 -0500

    Incluye la conexion a la BD

---
+++ funtions/conexionBD.py
@@ -0,0 +1,21 @@
+import _sqlite3
+import os
+
+DB_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "BD", "taller.db")
+
+def conectar():
+    try:
+        conn = _sqlite3.connect(DB_path)
+        cursor = conn.cursor()
+        print("Conexion exictosa")
+        return conn, cursor
+    except _sqlite3.Error as e:
+        return None, None
+    
+def desconectar(conn):
+    try:
+        if conn:
+            conn.close()
+            return True
+    except _sqlite3.Error as e:
+        return None
